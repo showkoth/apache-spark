4e456ed6c19e02b85f28e9f6c8ec0ba09ac70dd4
Ryan Nahm <rnahm@nd.edu>
2025-02-11 19:01:52 +0000

    query.py workign locally

--- testing/spark-app/query.py
+++ testing/spark-app/query.py
@@ -4,30 +4,39 @@ from pyspark.sql.functions import rand, lit, row_number
 from pyspark.sql.window import Window
 
 # Set environment variables for local mode
-os.environ['SPARK_LOCAL_IP'] = 'localhost'
-os.environ['SPARK_WORKER_CORES'] = '2'  # Adjust based on your machine
-os.environ['SPARK_WORKER_MEMORY'] = '2g'  # Adjust based on your machine
+os.environ["SPARK_LOCAL_IP"] = "localhost"
+os.environ["SPARK_WORKER_CORES"] = "2"  # Adjust based on your machine
+os.environ["SPARK_WORKER_MEMORY"] = "2g"  # Adjust based on your machine
 
-spark = (SparkSession.builder
-    .appName("SQL-Test")
-    .master("spark://localhost:7077")
-    .config("spark.sql.shuffle.partitions", "2")  # Reduce for small datasets
-    .config("spark.default.parallelism", "2")
+spark = (
+    SparkSession.builder.appName("Web3DB")
+    .master("local[*]")  # Use all available cores
     .config("spark.driver.memory", "2g")
     .config("spark.executor.memory", "2g")
+    .config("spark.sql.shuffle.partitions", "2")  # Reduce for small datasets
+    .config("spark.default.parallelism", "2")
+    .config("spark.driver.bindAddress", "localhost")
+    .config("spark.driver.host", "localhost")
     .config("spark.sql.adaptive.enabled", "true")
     .config("spark.memory.offHeap.enabled", "true")
     .config("spark.memory.offHeap.size", "1g")
     .enableHiveSupport()
     .getOrCreate()
 )
-spark.sql("CREATE DATABASE IF NOT EXISTS test")
 
-print("Verifying Database creation:")
+db_name = "default"
+table_name = "principals_table"
+
 spark.sql("SHOW DATABASES").show()
 
-spark.sql("USE test")
+# get db info
+spark.sql(f"SELECT COUNT(*) FROM {db_name}.{table_name}").show()
+spark.sql(f"DESCRIBE {db_name}.{table_name}").show()
 
+df = spark.sql(f"SELECT * FROM {db_name}.{table_name} WHERE job = 'writer'")
+df.show()
+print(df.count())
+exit()
 print("Creating employee table:")
 
 spark.sql("""
@@ -50,7 +59,7 @@ num_rows = 200000000
 df = spark.range(num_rows).select(
     row_number().over(Window.orderBy(lit(1))).alias("id"),
     (rand() * 1000000000).cast("int").alias("name"),
-    (rand() * 100000).cast("float").alias("salary")
+    (rand() * 100000).cast("float").alias("salary"),
 )
 df.show()
 print("Random Data generation complete.")
