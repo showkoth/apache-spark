dacb378d833b49565877512ed3da1c865390e5bf
Ryan Nahm <rnahm@nd.edu>
2025-02-21 17:06:05 -0500

    working non-persistent sql queries

--- testing/spark-app/build-db.py
+++ testing/spark-app/build-db.py
@@ -8,10 +8,10 @@ spark = (
     .config("spark.driver.blockManager.port", "10026")
     .config("spark.driver.port", "10027")
     .config("spark.driver.host", "129.74.152.201")
-    .enableHiveSupport()
+    .config("spark.hadoop.fs.defaultFS", "hdfs://129.74.152.201:9000")
+    .config("spark.sql.warehouse.dir", "hdfs://129.74.152.201:9000/data/warehouse")
     .getOrCreate()
 )
-
 
 tsv_file = sys.argv[1]
 table_name = "principals_table"
@@ -29,4 +29,11 @@ df.write.mode("overwrite").saveAsTable(f"{db_name}.{table_name}")
 spark.sql("SHOW TABLES").show()
 print("Data successfully written to table")
 
+
+# get db info
+spark.sql(f"SELECT COUNT(*) FROM {db_name}.{table_name}").show()
+spark.sql(f"DESCRIBE {db_name}.{table_name}").show()
+
+df = spark.sql(f"SELECT * FROM {db_name}.{table_name} WHERE job = 'writer'")
+df.show()
 spark.stop()
--- testing/spark-app/query.py
+++ testing/spark-app/query.py
@@ -1,28 +1,14 @@
-import os
+import sys
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import rand, lit, row_number
-from pyspark.sql.window import Window
-
-# Set environment variables for local mode
-os.environ["SPARK_LOCAL_IP"] = "localhost"
-os.environ["SPARK_WORKER_CORES"] = "2"  # Adjust based on your machine
-os.environ["SPARK_WORKER_MEMORY"] = "2g"  # Adjust based on your machine
-
 spark = (
-    SparkSession.builder.appName("Web3DB")
-    .config("spark.driver.memory", "2g")
-    .config("spark.executor.memory", "2g")
-    .config("spark.sql.shuffle.partitions", "2")  # Reduce for small datasets
-    .config("spark.default.parallelism", "2")
+    SparkSession.builder.appName("Build-DB")
+    .master("spark://129.74.152.201:7077")
     .config("spark.blockManager.port", "10025")
     .config("spark.driver.blockManager.port", "10026")
     .config("spark.driver.port", "10027")
-    .config("spark.driver.host", "172.23.153.11")
-    .config("spark.sql.adaptive.enabled", "true")
-    .config("spark.memory.offHeap.enabled", "true")
-    .config("spark.memory.offHeap.size", "1g")
-    .config("spark.sql.warehouse.dir", "./spark-warehouse")
-    .enableHiveSupport()
+    .config("spark.driver.host", "129.74.152.201")
+    .config("spark.hadoop.fs.defaultFS", "hdfs://129.74.152.201:9000")
+    .config("spark.sql.warehouse.dir", "hdfs://129.74.152.201:9000/data/warehouse")
     .getOrCreate()
 )
 
@@ -40,35 +26,3 @@ spark.sql(f"DESCRIBE {db_name}.{table_name}").show()
 df = spark.sql(f"SELECT * FROM {db_name}.{table_name} WHERE job = 'writer'")
 df.show()
 print(df.count())
-exit()
-print("Creating employee table:")
-
-spark.sql("""
-        CREATE TABLE IF NOT EXISTS employees (
-            id INT, 
-            name STRING,
-            salary FLOAT
-        )
-""")
-
-spark.sql("SHOW TABLES").show()
-print("Finished creating table")
-
-
-# generate random data
-print("Generating random data")
-num_rows = 200000000
-
-
-df = spark.range(num_rows).select(
-    row_number().over(Window.orderBy(lit(1))).alias("id"),
-    (rand() * 1000000000).cast("int").alias("name"),
-    (rand() * 100000).cast("float").alias("salary"),
-)
-df.show()
-print("Random Data generation complete.")
-
-df.write.insertInto("employees", overwrite=True)
-
-spark.sql("SELECT COUNT (*) FROM employees").show()
-spark.sql("SELECT * FROM employees LIMIT 10").show()
